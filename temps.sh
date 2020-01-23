57295cbab37a9df06b80e0076db86a4fe33795f2
Manik <mnk_400@ilcoud.com>
2020-01-23 02:11:17 -0500

    Added looping functionality

--- temps.sh
+++ temps.sh
@@ -1,19 +1,51 @@
 #!/bin/bash
 
-temp_c=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
+	var=$(($var+1))
+	temp_c=$(vcgencmd measure_temp | egrep -o '[0-9]*\.[0-9]*')
+	temp_f=$(echo "scale=1;(9/5)*$temp_c+32" | bc -l)
 
-temp_f=$(echo "scale=1;(9/5)*$temp_c+32" | bc -l)
-
-if [ "$1" == "-f" ]; then
-	if [ "$2" == "-v" ]; then
-		echo "$temp_f"
-	else	
-		echo "Current temperatute is $temp_f degrees Fahrenheit"
-	fi	
-else
-	if [ "$1" == "-v" ]; then
-		echo "$temp_c"
-	else	
-		echo "Current temperature is $temp_c degrees Celsius"
-	fi	
-fi	
+	if [ "$1" == "-f" ]; then
+		if [ "$2" == "-v" ]; then
+			if [ "$3" == "-r" ]; then
+				while true
+				do
+				echo "$temp_f"
+				sleep 2
+				done
+			else
+				echo "$temp_f"
+			fi	
+		else
+			if [ "$2" == "-r" ]; then
+				while true
+				do
+				echo "Current temperatute is $temp_f degrees Fahrenheit"
+				sleep 2
+				done
+			else
+				echo "Current temperatute is $temp_f degrees Fahrenheit"	
+			fi	
+		fi	
+	else
+		if [ "$1" == "-v" ]; then
+			if [ "$2" == "-r" ]; then
+				while true
+				do
+				echo "$temp_c"
+				sleep 2
+				done
+			else
+				echo "$temp_c"
+			fi	
+		else
+			if [ "$1" == "-r" ]; then
+				while true
+				do
+				echo "Current temperature is $temp_c degrees Celsius"
+				sleep 2
+				done		
+			else
+				echo "Current temperature is $temp_c degrees Celsius"
+			fi		
+		fi	
+	fi
